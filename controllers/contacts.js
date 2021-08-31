6e24e295c27bcb7646443e474f2e557703cabf41
mishastr77 <mishastr.1984@mail.ru>
2021-08-31 15:58:07 +0200

    hw-3
    hw3

---
+++ controllers/contacts.js
@@ -0,0 +1,108 @@
+const { Contact } = require("../model/contacts");
+
+const createContact = async (req, res, next) => {
+  try {
+    const result = await Contact.create(req.body);
+    res.status(201).json({
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await Contact.find({});
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    next();
+  }
+};
+
+const getContactById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Contact.findById(id);
+    if (!result) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    next();
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+    });
+    if (!result) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
+    res.status(201).json({
+      result,
+    });
+  } catch (error) {
+    next();
+  }
+};
+
+const updateStatusContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { favorite } = req.body;
+    const result = await Contact.findByIdAndUpdate(
+      id,
+      { favorite },
+      { new: true }
+    );
+    if (!result) {
+      res.status(400).json({
+        message: "missing field favorite",
+      });
+    }
+
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const removeContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await Contact.findByIdAndRemove({ _id: id });
+    if (!result) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
+    res.status(204).json({
+      result,
+    });
+  } catch (error) {
+    next();
+  }
+};
+
+module.exports = {
+  createContact,
+  getAllContacts,
+  getContactById,
+  updateContact,
+  removeContact,
+  updateStatusContact,
+};
